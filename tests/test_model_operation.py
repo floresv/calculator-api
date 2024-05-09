7474193244bd3a2df19b84ee449bf1cac05d0333
autoblack <floresv@users.noreply.github.com>
2024-05-09 21:24:33 +0000

    fixup: Format Python code with Black

--- tests/test_model_operation.py
+++ tests/test_model_operation.py
@@ -4,7 +4,7 @@ from mimesis import Finance
 from project import db
 from project.models.operation import Operation
 from tests.base import BaseTestCase
-from app.utils import add_operation # type: ignore
+from app.utils import add_operation  # type: ignore
 
 
 class TestOperationModel(BaseTestCase):
